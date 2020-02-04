3ab857f1ca401377c73946b947043ac9c2ff81e2
Ricky Salinas <rsalinas6721@gmail.com>
2020-02-04 13:06:18 -0500

    Preping Client Portion

--- otp_enc.c
+++ otp_enc.c
@@ -13,61 +13,61 @@ void error(const char *msg) { perror(msg); exit(0); }
 
 int main(int argc, char *argv[])
 {
-	int socketFD, portNumber, charsWritten, charsRead;	
-	struct sockaddr_in serverAddress;	
-	struct hostent* serverHostInfo;	
-	char message[100000];	
-	char key[100000];  
+	int socketFD, portNumber, charsWritten, charsRead;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+	char message[100000];
+	char key[100000];
 
 	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); }
 
-	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); 
-	portNumber = atoi(argv[3]); 
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress));
+	portNumber = atoi(argv[3]);
 	serverAddress.sin_family = AF_INET; 
-	serverAddress.sin_port = htons(portNumber); 
+	serverAddress.sin_port = htons(portNumber);
 	serverHostInfo = gethostbyname("localhost");
 	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
-	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); 
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
 
-	socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+	socketFD = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketFD < 0) error("CLIENT: ERROR opening socket");
 	int yes = 1;
 	setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) 
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0)
 		error("CLIENT: ERROR connecting");
 
     FILE *inMess;
     char cMess;
     int countMess = 0;
-    inMess = fopen(argv[1], "r");	
-    
-    fseek(inMess, 0L, SEEK_END);	
-    long int size = ftell(inMess);	
+    inMess = fopen(argv[1], "r");
+
+    fseek(inMess, 0L, SEEK_END);
+    long int size = ftell(inMess);
     fseek(inMess, 0L, SEEK_SET);
     if (size == 0){
 	printf("File is Empty\n");
 	exit(1);
     }
     cMess = fgetc(inMess);
-    while (cMess != EOF){	
-	if ((!isupper(cMess)) && (cMess != ' ') && (cMess != '\n')){	
+    while (cMess != EOF){
+	if ((!isupper(cMess)) && (cMess != ' ') && (cMess != '\n')){
 	    printf("Input contains bad characters.");
 	    exit(1);
 	}
         cMess = fgetc(inMess);
-        countMess++;	
+        countMess++;
     }
-	fclose(inMess);	
+	fclose(inMess);
 
     FILE *inKey;
     char cKey;
     int countKey = 0;
-    inKey = fopen(argv[2], "r");	
-    cKey = fgetc(inKey);		
+    inKey = fopen(argv[2], "r");
+    cKey = fgetc(inKey);
     while (cKey != EOF){
-        cKey = fgetc(inKey);		
-        countKey++;	
+        cKey = fgetc(inKey);
+        countKey++;
     }
 	fclose(inKey);
 
@@ -101,22 +101,22 @@ int main(int argc, char *argv[])
 	while(fgets(message, sizeof(message)-1, inMess));
     fclose(inMess);
 	int messLen = strlen(message);
-	charsWritten = send(socketFD, message, messLen+1, MSG_WAITALL);	
+	charsWritten = send(socketFD, message, messLen+1, MSG_WAITALL);
 	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
 	if (charsWritten < strlen(message)) printf("CLIENT: WARNING: Not all data written to socket!\n");
 
 
     inKey = fopen(argv[2], "r");
 	while(fgets(key, sizeof(key)-1, inKey));
-	fclose(inKey);	
-	int keyLen = strlen(key);	
-	charsWritten = send(socketFD, key, keyLen+1, MSG_WAITALL);	
-	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");	
+	fclose(inKey);
+	int keyLen = strlen(key);
+	charsWritten = send(socketFD, key, keyLen+1, MSG_WAITALL);
+	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
 	if (charsWritten < strlen(key)) printf("CLIENT: WARNING: Not all data written to socket!\n");
-	
-	char ciphertext[messLen];	
-	charsRead = recv(socketFD, ciphertext, messLen+1, MSG_WAITALL); 
-	if (charsRead < 0) error("ERROR reading from socket");	
+
+	char ciphertext[messLen];
+	charsRead = recv(socketFD, ciphertext, messLen+1, MSG_WAITALL);
+	if (charsRead < 0) error("ERROR reading from socket");
 
 	printf("%s\n", ciphertext); // Message is printed
 	close(socketFD);	// Socket is closed
