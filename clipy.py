a4cdb3dc4a5b20b1f84ae66dc48503c81e1887d0
Ricky Salinas <rsalinas6721@gmail.com>
2020-02-09 00:55:39 -0500

    Commented Portion Completed

--- clipy.py
+++ clipy.py
@@ -1,27 +1,36 @@
 import socket
 import sys
 
-
 def main():
+
+# Port Number, Handle, And Server Ip Address Are Retrieved From User
 	portNumber = input("Enter Port Number: ")
 	HANDLE = input("Enter Handle: ")
 	serverAddress = input("Enter Server IP: ")
+# Port Number is converted to integer
 	portNumber = int(portNumber)
+# Connection is established with server using socket function
 	establishedConnectionFD = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	establishedConnectionFD.connect((serverAddress, portNumber))
 	while(True):
+# Message Is Retrieved From User
 		message = input("Enter Message:>> ")
+# \quit is checked. Program exits if TRUE. If TRUE, \quit message is sent to server.
 		if (message == "\\quit"):
 			establishedConnectionFD.sendall(message.encode('utf-8'))
 			print("Terminating Connection")
 			break
+# Handle is concatenated to messages
 		message = HANDLE + ":> " + message
+# Message is sent to Server
 		establishedConnectionFD.sendall(message.encode('utf-8'))
 		print("Waiting on response...")
 		bytes = b''
+# Response is taken from server and converted to  a string.
 		while len(bytes) < 500:
 			bytes += establishedConnectionFD.recv(500)
 		bytes = bytes.decode('utf-8')
+# Response checked for "\quit". Program exits in TRUE.
 		if (bytes[0] == "\\" and bytes[1] == "q" and bytes[2] == "u" and bytes[3] == "i" and bytes[4] == "t"):
 			print("Server Terminated Connection")
 			exit(0)
--- server.c
+++ server.c
@@ -8,6 +8,9 @@
 #include <netinet/in.h>
 
 int main(int argc, char *argv[]){
+/*
+	Variables are initialized
+*/
 	int BUFFSIZE = 500;
 	char mess[BUFFSIZE];
 	char response[BUFFSIZE];
@@ -17,15 +20,21 @@ int main(int argc, char *argv[]){
 	int status;
 	pid_t pid;
 	struct sockaddr_in serverAddress, clientAddress;
+
+// Program verifies that it was run correctly. Example: "./programName portNumber"
 	if (argc < 2) {
 		fprintf(stderr,"Please Run %s Followed By Port Number\n", argv[0]); exit(1);
 	}
 	memset((char *)&serverAddress, '\0', sizeof(serverAddress));
+
+// Socket is set up. This portion was taken from an assignment in class CS340
 	portNumber = atoi(argv[1]);
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(portNumber);
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
 	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+// Socket is checked for potential error
 	if (listenSocketFD < 0){
 		printf("%s \n", "An Error Occured");
 		exit(1);
@@ -34,38 +43,53 @@ int main(int argc, char *argv[]){
 		printf("%s \n", "An Error Occured");
 		exit(1);
 	}
-	listen(listenSocketFD, 1);
+// 	listen(listenSocketFD, 1);
+// Program runs until SIGINT is received
 	while (1){
+// Program Listens for 1 Connection
 		listen(listenSocketFD, 1);
+// Client Address is taken and used to create connection with socket function
 		sizeOfClientInfo = sizeof(clientAddress);
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
+// Connection is checked for error.
 		if (establishedConnectionFD < 0){
 			printf("%s \n", "An Error Occured");
 			exit(1);
 		}
+// Variables are set to \0
 		bzero(mess, BUFFSIZE);
 		memset(clientHandle, '\0', 11);
 		memset(response, '\0', BUFFSIZE);
+// Program Forks
 		pid = fork();
+// Sqitch statement is used to determine parent and child processes
 		switch (pid){
-			case 0:{
+			case 0:{ssss
+	// Child process runs connection with client until the connection is terminated by either client or server
 				while(1){
+// Server reads first message from client. The message is checked for '\quit'.
 						read(establishedConnectionFD, mess, BUFFSIZE);
 						strtok(mess, "\n");
 						if (strcmp(mess, "\\quit") == 0){
 							printf("%s \n", "Terminating Connection");
 							break;
 						}
+// Message received from client is printed to console
 						printf("%s\n", mess);
+// Message variable is cleared
 						memset(mess, '\0', BUFFSIZE);
+
+// User is asked to send a message
 						printf("%s ", "Enter Message:>>");
 						fgets(response, BUFFSIZE, stdin);
 						strtok(response, "\n");
+// Message is checked for '\quit'. Program terminates if True
 						if (strcmp(response, "\\quit") == 0){
 							write(establishedConnectionFD, response, BUFFSIZE);
 							printf("%s \n", "Terminating Connection");
 							break;
 						}
+// Program sends message to client
 						write(establishedConnectionFD, response, BUFFSIZE);
 						memset(response, '\0', BUFFSIZE);
 						printf("%s \n", "Waiting on response...");
